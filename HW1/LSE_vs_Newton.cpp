f26ff9df71448d35ddbc6c605a4e6977985ddd08
Powgo <u9921222@gmail.com>
2020-09-27 13:39:39 +0800

    add mat_T & mat_mult

--- HW1/LSE_vs_Newton.cpp
+++ HW1/LSE_vs_Newton.cpp
@@ -29,20 +29,50 @@ vector<vector<double>> readCSV(string csvfile){
     return data;
 }
 
+vector<vector<double>> matrix_transpose(vector<vector<double>> matrix){
+    vector<vector<double>> tranposed_matrix;
+    for(int i = 0; i < matrix.at(0).size(); i++){
+        vector<double> row;
+        for(int j = 0; j < matrix.size(); j++){
+            row.push_back(matrix.at(j).at(i));
+        }
+        tranposed_matrix.push_back(row);
+    }
+    return tranposed_matrix;
+}
+
+vector<vector<double>> matrix_multiply(vector<vector<double>> matrixA,vector<vector<double>> matrixB){
+    vector<vector<double>> ret_matrix;
+    for(int i = 0; i < matrixA.size(); i++){
+        vector<double> row;
+        for(int j = 0; j < matrixB.at(0).size(); j++){
+            double mult = 0;
+            for(int k = 0; k < matrixB.size(); k++){
+                mult += matrixA.at(i).at(k) * matrixB.at(k).at(j);
+            }
+            row.push_back(mult);
+        }
+        ret_matrix.push_back(row);
+    }
+    return ret_matrix;
+}
+
+vector<vector<double>> matrix_inverse(vector<vector<double>> matrix){
+    vector<vector<double>> ret_matrix;
+    return ret_matrix;
+}
+
 namespace plt = matplotlibcpp;
 int main()
 {
-    vector<vector<double>> data = readCSV("raw.csv");
-    vector<double> x_array;
-    vector<double> y_array;
+    // vector<vector<double>> data = readCSV("raw.csv");
+    vector<vector<double>> data = readCSV("test.csv");
+    vector<vector<double>> tran_data = matrix_transpose(data);
 
-    for (int i = 0; i < data.size(); i++){
-        x_array.push_back(data.at(i).at(0));
-        y_array.push_back(data.at(i).at(1));
-        cout << data.at(i).at(0) << ',' << data.at(i).at(1) << endl;
-    }
-    plt::plot(x_array,y_array,"ro");
+    // plot raw data
+    plt::plot(tran_data.at(0),tran_data.at(1),"ro");
     plt::show();
+    vector<vector<double>> ATA = matrix_multiply(tran_data,data);
 
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
